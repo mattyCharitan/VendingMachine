e89670ebb775205a0bca69eb9b8b57bb43df4698
חריטן מאטיל < @F22>
2023-05-17 13:15:05 +0300

    Add project files.

---
+++ VendingMachine/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    public abstract class Product
+    {
+        public string productId;
+        public string productName;
+        public float price;
+    }
+}
